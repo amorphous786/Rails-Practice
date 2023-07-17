c6ef8e700b16a3702277329f72ea9cc37c59741a
amorphous786 <usamanadeem0786@gmail.com>
2023-07-17 13:26:32 +0500

    first controller and root route manipulation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    def hello
+        render html: "!Mi Amor <3¡"
+    end
+
+    def goodbye
+        render html: "Sayonara Mi Amor"
+    end
 end
